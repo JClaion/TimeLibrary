0527f425e332811ee60b0f53bc333e177b018d9f
JVBC743 <jvbc@email.com>
2024-08-24 20:29:55 -0400

    Consertado o erro da classe Email não enviar o bendito email.

--- views/index.php
+++ views/index.php
@@ -10,3 +10,62 @@
 
 // $banco = DB::getInstance();
 
+require_once "../classe/Email.php";
+
+use classe\Email;
+
+$obj = new Email();
+
+// public function emailGenerico(string $assunto, string $conteudo, string $destino, string $alt){
+
+//$obj->emailGenerico();
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+    <form action="index.php" method = "POST">
+        Assunto: <input type="text" name = "assunto"><br>
+        Conteúdo: <input type="text" name = "conteudo"><br>
+        Destino: <input type="text" name = "destino"><br>
+        <!-- Assunto: <input type="text" name = "assunto"><br> -->
+
+        <input type="submit" name = "enviar" value = "Enviar">
+
+    </form>
+
+    <?php
+        if(isset($_POST["enviar"])){
+            if(isset($_POST["assunto"]) && !empty($_POST["assunto"])){
+                if(isset($_POST["conteudo"]) && !empty($_POST["conteudo"])){
+                    if(isset($_POST["destino"]) && !empty($_POST["destino"])){
+
+                        $assunto = $_POST["assunto"];
+                        $conteudo = $_POST["conteudo"];
+                        $destino = $_POST["destino"];
+
+                        $teste = $obj->emailGenerico($assunto, $conteudo, $destino);
+
+                        if($teste == false){
+
+                            echo "Email FALHOU!!<br>";
+                        }else{
+
+                            echo "Email foi enviado!";
+                        }
+                    }
+                }
+            }
+        }
+    
+    ?>
+    
+</body>
+</html>
