73e9a1eaa1504e4f2e21ac0e2e2b8cc032dc400d
José Claion <joseclaionmartins@gmail.com>
2024-08-25 21:27:46 -0400

    separando parte repetitivas do html

---
+++ views/parts/footer.php
@@ -0,0 +1,4 @@
+</body>
+<script src="../../assets/public/bootstrap-5.3.3-dist/js/bootstrap.js"></script>
+<script src="../../assets/public/bootstrap-5.3.3-dist/js/bootstrap.bundle.min.js"></script>
+</html>
---
+++ views/parts/header.php
@@ -0,0 +1,16 @@
+<?php
+    error_reporting(E_ALL);
+    ini_set('display_errors', 1);
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Teste</title>
+    <link rel="stylesheet" href="../../assets/public/bootstrap-5.3.3-dist/css/bootstrap.css">
+    <link rel="stylesheet" href="../../assets/public/css/style.css">
+</head>
+<body>
+
