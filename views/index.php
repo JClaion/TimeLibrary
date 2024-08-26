38155cc896dd2079933cadefaf867b5ddb771cb1
José Claion <joseclaionmartins@gmail.com>
2024-08-25 21:27:55 -0400

    testando components

--- views/index.php
+++ views/index.php
@@ -1,42 +1,9 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Página Inicial</title>
-</head>
-<body>
+<?php include "parts/header.php"; ?>
 
-    
-</body>
-</html>
+<?php include "components/navbar.php"; ?>
 
-<!-- 
+<?php include "components/sidebar.php"; ?>
 
-    ================HIERARQUIA DOS DIRETÓRIOS DO SISTEMA================
+<?php include "parts/footer.php"; ?>
 
-    assets- assets são nossos recursos de folhas de estilo, scripts, fontes e imagens.
-    
-        private- Recursos que não podem aparecer no navegador.
-        
-        public- Recursos que devem aparecer no navegador.
-            css- Contém todos os arquivos CSS.
-            images- Contém todos os arquivos de imagem.
-            js- COntémtodos os arquivos JavaScript.
-            
-    classe- Contem todas as classes que serão usadas no sistema.
 
-    interfaces- Onde ficam as interfaces das classes.
-
-    services- Contém todo o backend do sistema / Serviços usados pelo front-end.
-
-    utils- Arquivos/Classes que contém códigos que serão usados muitas vezes Exemplo o Util Math do Java.
-
-    vendor- Contém todas as bibliotecas que podem ser utilizadas no sistema.
-
-    views- Contém todo o frontend do sistema.
-        admin- Contém as páginas que só o administrador pode acessar.
-        components- Componentes visuais que podem ser aplicados nas páginas web.
-        usuario- Contém as páginas que todos os usuário podem acessar.
-        
--->
